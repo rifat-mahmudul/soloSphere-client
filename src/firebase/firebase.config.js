6db31735c4385ee55e6aac64deb801f9ae1f9f54
Mahmudul Hasan Rifat <mahmudulhasanrifat220@gmail.com>
2024-11-09 09:42:45 +0600

    Navbar Done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCXWyoRtwjklkL6RMUUiiS9oxTQ6BnFdw8",
+    authDomain: "solosphere-d187c.firebaseapp.com",
+    projectId: "solosphere-d187c",
+    storageBucket: "solosphere-d187c.firebasestorage.app",
+    messagingSenderId: "2121668912",
+    appId: "1:2121668912:web:6650cc2dd5b26c97c650f5"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar"
 
 const Root = () => {
     return (
         <div>
-            This is Root
+            <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     )
 }
